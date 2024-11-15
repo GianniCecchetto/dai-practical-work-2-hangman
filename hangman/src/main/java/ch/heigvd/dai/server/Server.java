93e76f41811bc2652716c30ef5b7ba7756565c1f
nathan.tschantz <nathan.tschantz@hes-so.ch>
2024-11-15 10:40:21 +0100

    added course server structure

---
+++ hangman/src/main/java/ch/heigvd/dai/server/Server.java
@@ -0,0 +1,51 @@
+package ch.heigvd.dai.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    private static final int PORT = 1902;
+
+    private static final String TEXTUAL_DATA = "Test hangman server";
+
+    public static String END_OF_LINE = "\n";
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("[Server] Listening on port " + PORT);
+
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     Reader reader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+                     BufferedReader in = new BufferedReader(reader);
+                     Writer writer = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+                     BufferedWriter out = new BufferedWriter(writer)) {
+
+                    System.out.println(
+                            "[Server] New client connected from "
+                                    + socket.getInetAddress().getHostAddress()
+                                    + ":"
+                                    + socket.getPort());
+
+                    System.out.println("[Server] Received textual data from client: " + in.readLine());
+
+                    System.out.println("[Server] Sending textual data to client: " + TEXTUAL_DATA);
+
+                    out.write(TEXTUAL_DATA + END_OF_LINE);
+                    out.flush();
+
+                    System.out.println("[Server] Closing connection");
+
+                } catch (IOException e) {
+                    System.out.println("[Server] IO exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("[Server] IO exception: " + e);
+        }
+    }
+
+
+}
