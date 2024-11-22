37183bf8dfe3cdb3db77d7c45babd6fa51f643af
nathan.tschantz <nathan.tschantz@hes-so.ch>
2024-11-22 11:41:53 +0100

    basic command and response working(no game yet)

--- hangman/src/main/java/ch/heigvd/dai/server/Server.java
+++ hangman/src/main/java/ch/heigvd/dai/server/Server.java
@@ -1,15 +1,21 @@
 package ch.heigvd.dai.server;
 
+import ch.heigvd.dai.client.Client;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
-public class Server
-{
-    private int port;
+public class Server {
+    public enum Message {
+        GAMES,
+        GAMESTATE,
+        OK,
+        ERROR,
+    }
 
-    private static final String TEXTUAL_DATA = "Test hangman server";
+    private int port;
 
     public static String END_OF_LINE = "\n";
 
@@ -34,17 +40,74 @@ public class Server
                                     + ":"
                                     + socket.getPort());
 
-                    System.out.println("[Server] Received textual data from client: " + in.readLine());
+                    // Set game in progress
+                    boolean gameInProgress = true;
+                    String wordToguess = "test";
 
-                    System.out.println("[Server] Sending textual data to client: " + TEXTUAL_DATA);
+                    System.out.println("[Server] The word to guess is: " + wordToguess);
 
-                    out.write(TEXTUAL_DATA + END_OF_LINE);
-                    out.flush();
+                    while (!socket.isClosed()) {
+                        String clientRequest = in.readLine();
+
+
+                        if (clientRequest == null) {
+                            socket.close();
+                            continue;
+                        }
+
+                        String[] clientRequestParts = clientRequest.split(" ", 2);
+                        Client.Message message = null;
+                        try {
+                            message = Client.Message.valueOf(clientRequestParts[0]);
+                        } catch (Exception e) {
+                            // Do nothing
+                        }
+
+                        String response = "";
+                        System.out.println("[Server] Got a message: " + message);
+                        switch (message) {
+                            case JOIN -> {
+                                System.out.println("[Server] New client joined the game on " + clientRequestParts[1]);
+                                response = Message.OK + END_OF_LINE;
+                            }
+                            case LISTGAMES -> {
+                                if (gameInProgress) {
+                                    response =
+                                            Message.ERROR + " 1: a game is already launched" + END_OF_LINE;
+                                } else {
+                                    System.out.println("[Server] Sending game list");
+                                    response = "la list mdr" + END_OF_LINE;
+                                    //response = Message.OK + END_OF_LINE;
+                                }
+                            }
+                            case GUESS -> {
+                                if (clientRequestParts[1].length() > 50) {
+                                    response = Message.ERROR + " 2: more than 50 character" + END_OF_LINE;
+                                    break;
+                                }else if(clientRequestParts[1].isEmpty()) {
+                                    response = Message.ERROR + " 1: empty string" + END_OF_LINE;
+                                    break;
+                                }
+
+                                try {
+                                    response = Message.OK + END_OF_LINE;
+                                } catch (NumberFormatException e) {
+                                    response = Message.ERROR + " 2: the guess is not a character or a word" + END_OF_LINE;
+                                }
+                            }
+                            case null, default -> {
+                                response = Message.ERROR + " -1: invalid message" + END_OF_LINE;
+                            }
+                        }
+                       // System.out.println(response);
+                        out.write(response);
+                        out.flush();
+                    }
 
                     System.out.println("[Server] Closing connection");
-
                 } catch (IOException e) {
                     System.out.println("[Server] IO exception: " + e);
+                    //return 1;
                 }
             }
         } catch (IOException e) {
